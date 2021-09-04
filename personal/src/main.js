5c78b4861e7bb7142c67038d579082fb830a627f
bieluyang <980353763@qq.com>
2021-09-04 09:38:51 +0800

    修改路径

--- personal/src/main.js
+++ personal/src/main.js
@@ -4,8 +4,8 @@ import router from './router'
 import store from './store'
 import Vant from 'vant';
 import 'vant/lib/index.css';
-import './assets/字体/iconfont2.css'
-import './assets/字体/iconfont.css'
+import './assets/font/iconfont2.css'
+import './assets/font/iconfont.css'
 // import { Icon } from 'vant';
 // Vue.use(Icon);
 Vue.use(Vant);
