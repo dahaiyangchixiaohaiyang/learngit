e27cd21e3cfcfa888ae13358574067af0b46cd98
dahaiyang <980353763@qq.com>
2021-09-08 21:22:55 +0800

    详情页

--- personal/src/views/Detail.vue
+++ personal/src/views/Detail.vue
@@ -11,11 +11,81 @@
         关注
       </div>     
     </div>
-
+    <div class="contents">
+      <h1>容易热评的古风句子,适合发圈做书签</h1>
+      <span>2020-08-04 09:10</span>
+      <p>容易热评的古风句子,适合发圈做书签；今天古朴网小编为大家分享一组适合发朋友圈或者是拿来做书签的神仙古风句子，超级容易热评哟，有你心动的短句嘛，快来看看吧~</p>
+      <img src="img/userimg/37684471_1591506914.jpg" alt="">
+      <div class="connt">
+        <p>
+          阳春二三月，草与水同色。<br>
+          总之岁月漫长，然而值得等待。<br>
+          你再不来，我就下雪了。<br>
+          春水碧于天，画船听雨眠。<br>
+          轻罗小扇扑流萤。<br>
+          孤舟蓑笠翁，独钓寒江雪。<br>
+          风雪夜归人<br>
+          入月里我是瓶中的水，你是青天的云。<br>
+          理想只不过是个葬送我们一生的口号。<br>
+          然而人生辽阔，不要只活在爱恨里。<br>
+          
+        </p>
+        <img src="img/userimg/37684471_1591506914.jpg" alt="">
+        <p>
+          你说你孤独，像很久以前长星照耀十三个州府，你那种孤独。<br>
+          柳下闻瑶琴起舞和一曲，仿佛映当年翩若惊鸿影。<br>
+          只要想起一生中后悔的事，梅花便落满了南山。<br>
+          这世间的苦难不会比你的勇气多<br>
+          书不成字，纸短情长。<br>
+          我是一个在黑暗中大雪纷飞的人啊。<br>
+          这世上真话本就不多，一位女子的脸红胜过一大段对白。<br>
+          他们紧挨着孤独，像世间疾苦，又不掩春黄香时，等人来敲门。而敲门的人走错时间，也在等黄香。<br>
+          暮冬时烤雪，迟夏写长<br>
+        </p>
+      </div>
+    </div>
+    
   </div>
 </template>
 
 <style scoped lang="scss">
+.contents{
+  margin-top: 0.2rem;
+  padding: 0 0.3rem;
+  h1{
+    font-size: 0.33rem;
+    font-weight: 400;
+  }
+  span{
+    margin-top: 0.2rem;
+    padding-bottom: 0.2rem;
+    margin-bottom: 0.2rem;
+    border-bottom:1px solid #f5f5f5;
+    display: block;
+    font-size: 0.03rem;
+    color: #999;
+  }
+  p{
+    font-size: 4vw;
+  }
+  img{
+    margin-top: 0.2rem;
+    width: 100%;
+    border-radius: 50%;
+    display: block;
+  }
+  .connt{
+    margin-top: 0.2rem;
+    text-align: center;
+    img{
+      margin-top: 0.2rem;
+      border-radius: 0;
+    }
+    p{
+      margin-top: 0.2rem;
+    }
+  }
+}
 .debar{
   height: 0.7rem;
   background-color: #DE575D;
@@ -32,7 +102,6 @@
     position: relative;
     left: 4%;
     width: 15%;
-    text-align: center;
     height: 0.4rem;
     line-height: 0.4rem;
     font-size: 0.25rem;
